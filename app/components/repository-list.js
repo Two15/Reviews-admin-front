7fdb4b088cdfe0b024a762e06f068df683e0cfce
Xavier Cambar <xcambar@gmail.com>
2016-08-04 16:28:49 +0200

    Resets repositories when the org is changed

--- app/components/repository-list.js
+++ app/components/repository-list.js
@@ -1,15 +1,24 @@
 import Ember from 'ember';
 
 const { Component, computed, inject: { service } } = Ember;
+
+let repos = computed('organization', {
+  get() {
+    return this.get('ajax').orgRepos(this.get('organization'));
+  }
+});
+
 export default Component.extend({
   classNames: ['list', 'repository-list'],
   searchField: 'full_name',
   ajax: service(),
-  repos: computed('organization', {
-    get() {
-      return this.get('ajax').orgRepos(this.get('organization'));
-    }
-  }).readOnly(),
+  repos,
+  didUpdateAttrs() {
+    this._super(...arguments);
+    // Resets the repos
+    this.set('repos', repos);
+    this.set('selected', undefined);
+  },
   actions: {
     select(repo) {
       this.set('selected', repo);
